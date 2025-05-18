77b8ea3f5e8ca86512579fa25dffecfbb769dafb
Blank <chris.tong.muc@gmail.com>
2025-05-18 13:11:57 +0200

    stats passt

--- stats.py
+++ stats.py
@@ -3,4 +3,13 @@ def words(book_text):
         for i in book_text.split():
                 num_words += 1
         return num_words
+def characters(book_text):
+	character = {}
+	for i in book_text:
+		i = i.lower()
+		if i in character:
+			character[i] += 1
+		else: 
+			character[i] = 1
+	return character
 
