51638867901370566ff29266c1a3dac27e3aeee8
Blank <chris.tong.muc@gmail.com>
2025-05-18 10:52:16 +0200

    counting words

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(filepath):
+	with open(filepath) as f:
+		return f.read()
+def words(book_text):
+	num_words = 0
+	for i in book_text.split():
+		num_words += 1
+	return num_words
+
+
+def main():
+	book_text = get_book_text("books/frankenstein.txt")
+	num_words = words(book_text)
+	print(f"{num_words} words found in the document")
+
+main()
