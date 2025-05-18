e7242618874082d2743bab584778c064cb23f70c
Blank <chris.tong.muc@gmail.com>
2025-05-18 19:39:47 +0200

    pain just pain

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ def get_book_text(filepath):
 
 def main():
 	book_text = get_book_text("books/frankenstein.txt")
-	from stats import words
+	from stats import words, characters, sort_list
 	num_words = words(book_text)
-	from stats import characters
 	character = characters(book_text)
-	print(f"{num_words} words found in the document")
-	print(character)
+	sorted_chars = sort_list(character)
+	print("============ BOOKBOT ============")
+	print(f"Analyzing book found at books/frankenstein.txt...")
+	print("----------- Word Count ----------")
+	print(f"Found {num_words} total words")
+	print("--------- Character Count -------")
+	for char_dict in sorted_chars:
+		char = char_dict["char"]
+		if char.isalpha():
+			print(f"{char}: {char_dict['num']}")
+	print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -3,6 +3,8 @@ def words(book_text):
         for i in book_text.split():
                 num_words += 1
         return num_words
+
+
 def characters(book_text):
 	character = {}
 	for i in book_text:
@@ -12,4 +14,17 @@ def characters(book_text):
 		else: 
 			character[i] = 1
 	return character
+
+
+def sort_on(dict):
+	return dict["num"]
+def sort_list(char_dict):
+	sorted_list = []
+	for char, count in char_dict.items():
+		sort = {"char": char, "num": count}
+		sorted_list.append(sort)
+	sorted_list.sort(reverse = True, key = sort_on)
+	return sorted_list 
+
+
 
