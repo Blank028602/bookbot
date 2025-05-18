cea015657ea2e553c84ed899f2322baaeab1dedf
Blank <chris.tong.muc@gmail.com>
2025-05-18 13:13:57 +0200

    zählt alle characters

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ def main():
 	book_text = get_book_text("books/frankenstein.txt")
 	from stats import words
 	num_words = words(book_text)
+	from stats import characters
+	character = characters(book_text)
 	print(f"{num_words} words found in the document")
+	print(character)
 
 main()
 
